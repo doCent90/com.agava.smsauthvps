2871b7368c592bf1e837f2da7f8382fe02bdaf31
marriuss <53435883+marriuss@users.noreply.github.com>
2024-06-11 16:23:58 +0400

    Fix number input

--- Runtime/Services/WinkAcceessHelper.cs
+++ Runtime/Services/WinkAcceessHelper.cs
@@ -9,10 +9,11 @@ namespace Agava.Wink
     public static class WinkAcceessHelper
     {
         /// <summary>
-        ///     Return formated number after enter.
+        ///     Return formated number after enter
         /// </summary>
         public static string GetNumber(string phone_number, int minNumberCount, int maxNumberCount, bool additivePlusChar)
         {
+            phone_number = Regex.Replace(phone_number, @"<[^>]*>", string.Empty);
             phone_number = Regex.Replace(phone_number, "[^0-9]", string.Empty);
 
             bool isCorrectNumber = ulong.TryParse(phone_number, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out ulong resultNumber);
