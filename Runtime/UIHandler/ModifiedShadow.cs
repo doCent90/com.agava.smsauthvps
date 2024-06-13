2a045af1b373fc400cdf7ea2f5faf6b120a4aefd
marriuss <53435883+marriuss@users.noreply.github.com>
2024-06-14 02:34:40 +0400

    Add shaders

---
+++ Runtime/UIHandler/ModifiedShadow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Agava.Wink
+{
+    internal class ModifiedShadow : Shadow
+    {
+        public override void ModifyMesh(VertexHelper vh)
+
+        {
+            if (!this.IsActive())
+                return;
+
+            var list = ListPool<UIVertex>.Get();
+            vh.GetUIVertexStream(list);
+
+            ModifyVertices(list);
+
+#if UNITY_5_2_1pX || UNITY_5_3_OR_NEWER
+            vh.Clear();
+#endif
+            vh.AddUIVertexTriangleStream(list);
+            ListPool<UIVertex>.Release(list);
+        }
+
+        public virtual void ModifyVertices(List<UIVertex> verts)
+        {
+        }
+    }
+}
