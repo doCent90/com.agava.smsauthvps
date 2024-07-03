80fadeb36f163570b475948f6c34d83e5ed722c2
marriuss <53435883+marriuss@users.noreply.github.com>
2024-07-03 20:28:52 +0400

    Add WinkTextButton.cs

---
+++ Runtime/UIHandler/WinkTextButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Agava.Wink;
+
+public class WinkTextButton : MonoBehaviour
+{
+    [SerializeField] private Text _signIn;
+    [SerializeField] private Text _subscribe;
+
+    private bool _authorized => WinkAccessManager.Instance.Authenficated;
+
+    private void Update()
+    {
+        _signIn.gameObject.SetActive(_authorized == false);
+        _subscribe.gameObject.SetActive(_authorized);
+    }
+}
